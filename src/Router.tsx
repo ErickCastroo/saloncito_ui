0bbeb4430db08b2bedfc9f74f0364afff3a03b44
zchelalo <eduardosaavedra687@gmail.com>
2024-09-06 00:48:41 -0700

    add: auth provider to router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import { AuthProvider } from '@/contexts/auth'
+
+import { AuthRoute } from '@/contexts/auth/AuthRoute'
+import { PublicRoute } from '@/contexts/auth/PublicRoute'
 import { Layout } from '@/components/Layout'
 import { Home } from '@/modules/home/ui/pages/Home'
 import { SignIn } from '@/modules/auth/ui/pages/SignIn'
@@ -7,18 +11,28 @@ import { SignIn } from '@/modules/auth/ui/pages/SignIn'
 function Router() {
   return (
     <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route
-            path='/'
-            element={<Home />}
-          />
-          <Route
-            path='/sign-in'
-            element={<SignIn />}
-          />
-        </Routes>
-      </Layout>
+      <AuthProvider>
+        <Layout>
+          <Routes>
+            <Route
+              path='/'
+              element={(
+                <AuthRoute>
+                  <Home />
+                </AuthRoute>
+              )}
+            />
+            <Route
+              path='/sign-in'
+              element={(
+                <PublicRoute>
+                  <SignIn />
+                </PublicRoute>
+              )}
+            />
+          </Routes>
+        </Layout>
+      </AuthProvider>
     </BrowserRouter>
   )
 }
