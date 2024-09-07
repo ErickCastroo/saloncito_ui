39fd65d2550c10b606dfdecf74bb70835e438eb9
zchelalo <eduardosaavedra687@gmail.com>
2024-09-06 20:51:03 -0700

    fix: input component, now use forwardRef

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,3 +1,4 @@
+import { forwardRef } from 'react'
 import { cn } from '@/utils/styles'
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
@@ -5,21 +6,22 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   className?: string
 }
 
-function Input({
-  type = 'text',
-  className = '',
-  styles,
-  ...props
-}: InputProps) {
-  return (
-    <input
-      type={type}
-      className={cn('bg text p-2 rounded', className)}
-      style={styles}
-      {...props}
-    />
-  )
-
-}
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({
+    type = 'text',
+    className = '',
+    styles,
+    ...props
+  }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn('bg text p-2 rounded', className)}
+        style={styles}
+        ref={ref}
+        {...props}
+      />
+    )
+  })
 
 export { Input }
