25fdfe5a836e374b4e0b9beadbb0b4926f6b6dfc
ErickCastroo <castrocamachoerickmiguel21@gmail.com>
2024-10-01 23:43:20 -0700

    add: image of Sonora México

--- src/modules/auth/ui/pages/SignUp/components/BrandContainer.tsx
+++ src/modules/auth/ui/pages/SignUp/components/BrandContainer.tsx
@@ -1,7 +1,8 @@
+import GobiernoSonora from '@/assets/images/Gobierno_de_Sonora.jpg'
 function BrandContainer() {
   return (
     <div className='bg-tertiary hidden w-1/2 h-5/6 md:flex items-center justify-center rounded-tr rounded-br'>
-      <img className='w-72 h-72' src='https://picsum.photos/200' alt='logo' />
+      <img className='w-[600px] h-[600px] object-contain' src={GobiernoSonora} alt='Logo of Saloncito' />
     </div>
   )
 }
