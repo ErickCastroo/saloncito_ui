ab515da1a2e4b2777114bc372105e44e601f5408
ErickCastroo <castrocamachoerickmiguel21@gmail.com>
2024-10-18 19:32:17 -0700

    add:Create miniCard for Join or create class

--- src/components/Layout/components/ClassButton.tsx
+++ src/components/Layout/components/ClassButton.tsx
@@ -1,13 +1,43 @@
+import { useState } from 'react'
+
+import { useTranslation } from 'react-i18next' 
+
 import { HiOutlinePlus } from 'react-icons/hi2'
 
 function ClassButton() {
+
+  const { t } = useTranslation()
+
+  const [isCardVisible, setIsCardVisible] = useState(false)
+
+  const toggleCardVisibility = () => {
+    setIsCardVisible(!isCardVisible)
+  }
+
+
   return (
-    <button
-      type='button'
-      className='bg-secondary text-secondary absolute bottom-0 right-0 rounded-full text-3xl p-3 mb-6 mr-6 z-10'
-    >
-      <HiOutlinePlus />
-    </button>
+    <>
+      {
+        isCardVisible && (
+          <div className="absolute flex flex-col bottom-14 right-8 bg-white text-xl p-3 mb-6 mr-6 rounded-md">
+            <button className="text-left px-4 py-2 w-full rounded-sm hover:bg-blue-50">
+              {t('joinClass')}
+            </button>
+            <button className="px-4 py-2 w-full rounded-sm hover:bg-blue-50">
+              {t('createClass')}
+            </button>
+          </div>
+        )
+      }
+      <button
+        type='button'
+        onClick={toggleCardVisibility}
+        className='bg-secondary text-secondary absolute bottom-0 right-0 rounded-full text-3xl p-3 mb-6 mr-6 z-10'
+      >
+        <HiOutlinePlus />
+      </button>
+
+    </>
   )
 }
 
--- src/modules/home/ui/pages/Home.tsx
+++ src/modules/home/ui/pages/Home.tsx
@@ -1,6 +1,5 @@
 import { Card } from '@/components/Card'
 
-
 function Home() {
 
   return (
