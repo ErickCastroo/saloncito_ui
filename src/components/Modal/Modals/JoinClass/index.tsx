53b1a50ae103a86e31718bc3796cfbf3843047de
ErickCastroo <castrocamachoerickmiguel21@gmail.com>
2024-10-26 01:43:47 -0700

    add: JoinClass modal component styles for improved visibility and usability

--- src/components/Modal/Modals/JoinClass/index.tsx
+++ src/components/Modal/Modals/JoinClass/index.tsx
@@ -1,4 +1,5 @@
 import Modal from '@/components/Modal'
+
 import { useTranslation } from 'react-i18next'
 
 function JoinClass({ active, toggle }: { active: boolean; toggle: () => void }) {
@@ -6,10 +7,32 @@ function JoinClass({ active, toggle }: { active: boolean; toggle: () => void }) 
 
   return (
     <Modal active={active} toggle={toggle}>
-      <div>
-        <h2>{t('joinClass')}</h2>
-        <p>Introduce el código de la clase para unirte.</p>
-        <input type='text' placeholder='Código de clase' className='border p-2 mt-2' />
+      <div className="w-full h-full overflow-y-auto p-5"> 
+        <h2 className='text-xl text-black mb-5'>{t('joinClass')}</h2>
+
+        <div className='border w-full h-auto text-black p-4'>
+          <h3 className='flex items-start justify-start mt-5 text-lg'>Código de la clase</h3>
+          <span className='flex items-start justify-start text-gray-400 text-sm mb-5'>
+            Pídele a tu profesor el código de la clase y, luego, ingrésalo aquí
+          </span>
+          <input
+            type='text'
+            placeholder='Código de la clase'
+            className='border-2 border-black w-full h-full rounded-md text-black p-2 mb-5'
+          />
+        </div>
+
+        <p className='text-black mt-5 mb-5'>Para acceder con un código de la clase</p>
+        <ul className="list-disc pl-5">
+          <li className='text-black mb-2'>Usa una cuenta autorizada.</li>
+          <li className='text-black mb-2'>Usa un código de la clase que tenga entre 5 y 7 letras o números, sin espacios ni símbolos.</li>
+          <li className='text-black mb-2'>Si tienes problemas para unirte a la clase, pídele ayuda a tu profesor.</li>
+        </ul>
+
+        <div className="flex flex-row items-end justify-end">
+          <button className='text-black p-2 rounded-md mt-5 md:mr-2'>{t('cancel')}</button>
+          <button className='text-black p-2 rounded-md mt-5'>{t('joinClass')}</button>
+        </div>
       </div>
     </Modal>
   )
