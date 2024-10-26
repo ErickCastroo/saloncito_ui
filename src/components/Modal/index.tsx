10c4a3ec30cac173ffff5a3a318c2728e8eff293
ErickCastroo <castrocamachoerickmiguel21@gmail.com>
2024-10-26 00:46:55 -0700

    Refactor Modal component styles for improved visibility

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -16,9 +16,9 @@ export default class Modal extends Component<ModalProps>  {
       <Portal>
         {
           active && (
-            <div className='fixed top-0  left-0 w-full h-full text-slate-200 bg-black bg-opacity-60 flex justify-center items-center'>
-              <div className='relative  dark:bg-slate-900 text-slate-200 border border-gray-200 dark:border-gray-400 rounded-sm bg-white w-[70%] h-[70%] p-20 rounded-5 shadow-md'>
-                <button onClick={toggle} className='absolute top-0 right-0 bg-transparent border-none text-2xl px-6 py-5 cursor-pointer'>
+            <div className='fixed inset-0 flex justify-center items-center transition-colors'>
+              <div className='relative  dark:bg-slate-900 text-slate-200 border border-gray-200 dark:border-gray-400 rounded-sm bg-white w-[70%] h-[70%] px-6 py-5  rounded-5 shadow-md'>
+                <button onClick={toggle} className='text-black absolute top-0 right-0 bg-transparent border-none text-2xl px-6 py-5 cursor-pointer'>
                   <IoCloseSharp />
                 </button>
                 {children}
