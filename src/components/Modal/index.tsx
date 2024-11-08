7b73e10760796ed3a0e89b6ffcfdd7dcdf38c63e
ErickCastroo <castrocamachoerickmiguel21@gmail.com>
2024-11-07 17:51:34 -0700

    fix: bg-transparent

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -16,7 +16,7 @@ export default class Modal extends Component<ModalProps>  {
       <Portal>
         {
           active && (
-            <div className='fixed inset-0 flex justify-center items-center transition-colors'>
+            <div className='fixed inset-0 flex justify-center items-center transition-colors bg-black/70'>
               <div className='relative  dark:bg-slate-900 text-slate-200 border border-gray-200 dark:border-gray-400 rounded-sm bg-white w-[70%] h-[70%] px-6 py-5  rounded-5 shadow-md'>
                 <button onClick={toggle} className='text-black absolute top-0 right-0 bg-transparent border-none text-2xl px-6 py-5 cursor-pointer'>
                   <IoCloseSharp />
